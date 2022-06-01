66fe9e10d70d1bc494518f481b19bb5120ed481a
Ayoub Moudrika <amoudrika@syndics.ca>
2022-05-31 22:56:00 -0400

    squelette terminé

--- site.js
+++ site.js
@@ -6,3 +6,16 @@ function openTab(tabName) {
     }
     document.getElementById(tabName).style.display = "block";
 }
+
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
